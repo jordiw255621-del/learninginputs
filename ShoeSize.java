3e76f58b3be84f9648ccc83523c6d3f5c9cba3c2
Jordan Wong <jordiw255621@gmail.com>
2026-03-11 14:08:16 +1100

    Added an extra step of asking user for gender, this was done by utilising (if choice == 1) and (if choice == 2) statements. This allows the user to choose which shoe size conversion they want to do.

--- ShoeSize.java
+++ ShoeSize.java
@@ -1,19 +1,44 @@
 public class ShoeSize {
     public static void main(String[] args) {
-        System.out.print("Enter your foot size in cm: ");
-        double shoeSize = In.nextDouble();
-        if (shoeSize < 0) {
-            System.out.println("Your shoe size can't be negative");
-        } else if (shoeSize <= 1) {
-            System.out.println("Your shoe size can't be zero");
-        } else if (shoeSize <= 22) {
-            System.out.println("Your shoe size in US is Woman's 4 or less");
-        } else if (shoeSize <= 23) {
-            System.out.println("Your shoe size in US is Woman's 5 to 7");
-        } else if (shoeSize <= 25.1) {
-            System.out.println("Your shoe size in US is Woman's 8 to 10");
+        System.out.println("What is your gender" + "\n1. Male" + "\n2. Female");
+
+        int choice = In.nextInt();
+        if (choice == 1) {
+            System.out.println("You chose to convert your shoe size for men.");
+            System.out.print("Enter your foot size in cm: ");
+            double shoeSize = In.nextDouble();
+            if (shoeSize < 0) {
+                System.out.println("Your shoe size can't be negative");
+            } else if (shoeSize == 0) {
+                System.out.println("Your shoe size can't be zero");
+            } else if (shoeSize <= 24) {
+                System.out.println("Your shoe size in US is Man's 6 or less");
+            } else if (shoeSize <= 25.5) {
+                System.out.println("Your shoe size in US is Man's 7 to 9");
+            } else if (shoeSize <= 27) {
+                System.out.println("Your shoe size in US is Man's 10 to 12");
+            } else {
+                System.out.println("Your shoe size in US is Man's 12+");
+            }
+        } else if (choice == 2) {
+            System.out.println("You chose to convert your shoe size for women.");
+            System.out.print("Enter your foot size in cm: ");
+            double shoeSize = In.nextDouble();
+            if (shoeSize < 0) {
+                System.out.println("Your shoe size can't be negative");
+            } else if (shoeSize == 0) {
+                System.out.println("Your shoe size can't be zero");
+            } else if (shoeSize <= 22) {
+                System.out.println("Your shoe size in US is Woman's 4 or less");
+            } else if (shoeSize <= 23) {
+                System.out.println("Your shoe size in US is Woman's 5 to 7");
+            } else if (shoeSize <= 25.1) {
+                System.out.println("Your shoe size in US is Woman's 8 to 10");
+            } else {
+                System.out.println("Your shoe size in US is Woman's 10+");
+            }
         } else {
-            System.out.println("Your shoe size in US is Woman's 10+");
+            System.out.println("Invalid choice.");
         }
     }
 }
