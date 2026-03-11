5e46c6e826cfdad8193524b984dab8287d8847c2
Jordan Wong <jordiw255621@gmail.com>
2026-03-12 09:25:10 +1100

    Utilised <= instead of < and > in the if statement to include the age of 11 and 18 in the correct categories.

--- childoradult.java
+++ childoradult.java
@@ -6,12 +6,11 @@ public class childoradult {
             System.out.println("Your age can't be negative");
         } else if (age == 0) {
             System.out.println("Your age can't be zero");
-        } else if (age < 11) {
+        } else if (age <= 11) {
             System.out.println("You are a Child.");
-        } else if (age > 12 && age < 18) {
+        } else if (age <= 17) {
             System.out.println("You are a Tween.");
-        }
-        if (age >= 18) {
+        } else {
             System.out.println("You are an Adult.");
         }
     }
